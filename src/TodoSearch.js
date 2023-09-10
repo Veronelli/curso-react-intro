eb96fb3410b9f2f03bf44847a55740a2f100f0fe
Veronelli <facu2000veronelli@outlook.com>
2023-09-09 23:51:41 -0300

    add event

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,8 +1,16 @@
 import './css/TodoSearch.css'
 
+function searchTodo(event){
+    searchField = event.target.value
+    console.log(searchField)
+}
+let searchField="Test"
 function TodoSearch(){
     return (
-        <input placeholder="Cortar Cebolla">
+        <input placeholder="Cortar Cebolla"
+        value={searchField}
+        onChange={(event)=>searchTodo(event)}
+        >
             
         </input>
     )
