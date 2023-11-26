ed13b4f42700fcfc5180728fb7d27b276db5db30
Veronelli <facu2000veronelli@outlook.com>
2023-11-25 21:28:21 -0300

    delete console.log

--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -1,12 +1,11 @@
 import "./TodoList.css";
 
 function TodoList(props) {
-  console.log(">>>>>>>>>>>>>",props)
   return (
     <section className="TodoList-container">
       {props.error && props.onError()}
       {props.loading && props.onLoading()}
-      {(!props.loading && props.searchedTodos.length === 0) && props.onEmptyTodos()}
+      {(!props.loading && props.searchedTodos?.length === 0) && props.onEmptyTodos()}
       {props.searchedTodos.map(props.render)}
 
     </section>
